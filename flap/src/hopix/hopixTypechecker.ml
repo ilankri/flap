e3c592ad9500d696c5a46e94595cd32006463fc7
Yann Regis-Gianas <yrg@irif.fr>
2017-01-31 22:56:00 +0100

    TP2.

--- flap/src/hopix/hopixTypechecker.ml
+++ flap/src/hopix/hopixTypechecker.ml
@@ -1,19 +1,152 @@
-(** This module implements a type checker for Datix. *)
+(** This module implements a type checker for Hopix. *)
 open HopixAST
 open HopixTypes
 open HopixPrettyPrinter
 
 let initial_typing_environment = HopixTypes.initial_typing_environment
+
 type typing_environment = HopixTypes.typing_environment
 
 let type_error = Error.error "typechecking"
 
 let located f x = f (Position.position x) (Position.value x)
 
+(** [check_program_is_fully_annotated ast] traverses the [ast] of the
+    program and for each definition, it makes sure that bound variables
+    have a declared types and that recursive functions also have a type
+    annotation to declare their return type. *)
+let check_program_is_fully_annotated ast =
+
+  let rec program p = List.iter (located definition) p
+
+  and definition pos = function
+    | DefineValue (x, e) ->
+      failwith "Students! This is your job!"
+    | DefineRecFuns recdefs ->
+      List.iter (fun (_, fdef) -> located function_definition fdef) recdefs
+    | _ ->
+      ()
+  and function_definition pos = function
+    | FunctionDefinition (_, ps, e) ->
+      failwith "Students! This is your job!"
+
+  and expression pos = function
+    | Define (_, e1, e2) ->
+      failwith "Students! This is your job!"
+    | DefineRec (recdefs, e) ->
+      failwith "Students! This is your job!"
+    | Apply (a, _, args) ->
+      failwith "Students! This is your job!"
+    | If (cts, f) ->
+      failwith "Students! This is your job!"
+    | Fun fdef ->
+      failwith "Students! This is your job!"
+    | Tagged (_, _, es) ->
+      failwith "Students! This is your job!"
+    | Case (e, bs) ->
+      failwith "Students! This is your job!"
+    | TypeAnnotation (e, _) | Ref e | Read e ->
+      failwith "Students! This is your job!"
+    | Write (e1, e2) | While (e1, e2) ->
+      failwith "Students! This is your job!"
+    | Literal _ | Variable _ ->
+      failwith "Students! This is your job!"
+  and pattern pos = function
+    | PTypeAnnotation ({ Position.value = PVariable _ }, _) ->
+      failwith "Students! This is your job!"
+    | PTypeAnnotation (p, _) ->
+      failwith "Students! This is your job!"
+    | PVariable _ ->
+      failwith "Students! This is your job!"
+    | PTaggedValue (_, ps) | POr ps | PAnd ps ->
+      failwith "Students! This is your job!"
+    | PWildcard | PLiteral _ ->
+      failwith "Students! This is your job!"
+  and branch pos = function
+    | Branch (p, e) ->
+      failwith "Students! This is your job!"
+  and missing_type_annotation pos =
+    Error.error "typechecking" pos "A type annotation is missing."
+  in
+  program ast
+
 (** [typecheck tenv ast] checks that [ast] is a well-formed program
     under the typing environment [tenv]. *)
-let typecheck tenv ast =
-  ()
+let typecheck tenv ast : typing_environment =
+
+  check_program_is_fully_annotated ast;
+
+  let rec program p =
+    List.fold_left (fun env x -> located (definition env) x) tenv p
+
+  and definition tenv pos = function
+    | DefineValue (x, e) ->
+      bind_value (Position.value x) (
+	located (type_of_expression tenv) e
+      ) tenv
+    | DefineRecFuns recdefs ->
+      failwith "Student! This is your job!"
+    | _ ->
+      failwith "Student! This is your job!"
+
+  and function_definition pos tenv = function
+    | FunctionDefinition (_, ps, e) ->
+      failwith "Student! This is your job!"
+
+  and type_of_expression pos tenv = function
+    | Define (x, e1, e2) ->
+      failwith "Student! This is your job!"
+    | TypeAnnotation (e, ty) ->
+      failwith "Student! This is your job!"
+    | DefineRec (recdefs, e) ->
+      failwith "Student! This is your job!"
+    | Apply (a, types, args) ->
+      failwith "Student! This is your job!"
+    | If (cts, f) ->
+      failwith "Student! This is your job!"
+    | Fun fdef ->
+      failwith "Student! This is your job!"
+    | Tagged (_, _, es) ->
+      failwith "Student! This is your job!"
+    | Case (e, bs) ->
+      failwith "Student! This is your job!"
+    | Ref e ->
+      failwith "Student! This is your job!"
+    | Read e ->
+      failwith "Student! This is your job!"
+    | Write (e1, e2) ->
+      failwith "Student! This is your job!"
+    | While (e1, e2) ->
+      failwith "Student! This is your job!"
+    | Literal l ->
+      failwith "Student! This is your job!"
+    | Variable x ->
+      failwith "Student! This is your job!"
+
+  and type_of_literal pos = function
+    | LInt _ -> hint
+    | LString _ -> hstring
+    | LChar _ -> hchar
+
+  and pattern pos tenv = function
+    | PTypeAnnotation ({ Position.value = PVariable _ }, _) ->
+      failwith "Student! This is your job!"
+    | PTypeAnnotation (p, _) ->
+      failwith "Student! This is your job!"
+    | PVariable _ ->
+      assert false (* by check_program_is_fully_annotated. *)
+    | PTaggedValue (_, ps) | POr ps | PAnd ps ->
+      failwith "Student! This is your job!"
+    | PWildcard ->
+      failwith "Student! This is your job!"
+    | PLiteral _ ->
+      failwith "Student! This is your job!"
+
+  and branch pos = function
+    | Branch (p, e) ->
+      failwith "Student! This is your job!"
+  in
+  program ast
 
 let print_typing_environment =
   HopixTypes.print_typing_environment
--- flap/src/hopix/hopixTypes.ml
+++ flap/src/hopix/hopixTypes.ml
@@ -1,6 +1,183 @@
-type typing_environment = unit
+open HopixAST
 
-let initial_typing_environment () = ()
+(** Abstract syntax for types.
 
-let print_typing_environment tenv = ""
+    The following internal syntax for types is isomorphic
+    to the type [ty] defined in {!HopixAST} except that
+    all positions have been erased.
+
+*)
+type aty =
+  | ATyVar   of type_variable
+  | ATyCon   of type_constructor * aty list
+  | ATyArrow of aty list * aty
+
+let rec aty_of_ty = function
+  | TyVar x            -> ATyVar x
+  | TyCon (t, ts)      -> ATyCon (t, List.map aty_of_ty' ts)
+  | TyArrow (ins, out) -> ATyArrow (List.map aty_of_ty' ins, aty_of_ty' out)
+and aty_of_ty' x = aty_of_ty (Position.value x)
+
+let tvar x =
+  ATyVar (TId x)
+
+let ( --> ) tys ty =
+  ATyArrow (tys, ty)
+
+let constant x = TCon x, ATyCon (TCon x, [])
+let tcunit,   hunit    = constant "unit"
+let tcbool,   hbool    = constant "bool"
+let tcint,    hint     = constant "int"
+let tcstring, hstring  = constant "string"
+let tcchar,   hchar    = constant "char"
+
+module TypeVariableSet = Set.Make (struct
+  type t = type_variable
+  let compare = compare
+end)
+
+let free_type_variables ty =
+  let rec aux accu = function
+    | ATyVar tv -> TypeVariableSet.add tv accu
+    | ATyCon (_, tys) -> aux' accu tys
+    | ATyArrow (ins, out) -> aux' (aux accu out) ins
+  and aux' accu = function
+    | [] -> accu
+    | ty :: tys -> aux' (aux accu ty) tys
+  in
+  TypeVariableSet.elements (aux TypeVariableSet.empty ty)
+
+type aty_scheme = Scheme of type_variable list * aty
+
+let mk_type_scheme ty =
+  Scheme (free_type_variables ty, ty)
+
+let monotype ty =
+  Scheme ([], ty)
+
+type typing_environment = {
+  values            : (identifier * aty_scheme) list;
+  constructors      : (constructor * aty_scheme) list;
+  type_constructors : (type_constructor * type_information) list;
+  type_variables    : type_variable list;
+}
+and type_information = {
+  arity             : int;
+  data_constructors : constructor list
+}
+
+exception UnboundTypeConstructor of Position.position * type_constructor
+
+let check_well_formed_type env ty =
+  (* FIXME: Wrong! Student, fix this! *)
+  ()
+
+let internalize_ty env ty =
+  check_well_formed_type env ty;
+  aty_of_ty ty
+
+let empty_typing_environment = {
+  values = [];
+  constructors = [];
+  type_constructors = [];
+  type_variables = []
+}
+
+exception AlreadyBoundTypeVariable of Position.position * type_variable
+
+let bind_type_variable pos env tv =
+  if List.mem tv env.type_variables then
+    raise (AlreadyBoundTypeVariable (pos, tv));
+  { env with type_variables = tv :: env.type_variables }
+
+let is_type_variable_defined pos env tv =
+  List.mem tv env.type_variables
+
+let bind_value x scheme env = {
+  env with values = (x, scheme) :: env.values
+}
+
+exception UnboundIdentifier of Position.position * identifier
+
+let lookup_type_scheme_of_value pos x env =
+  try
+    List.assoc x env.values
+  with Not_found ->
+    raise (UnboundIdentifier (pos, x))
+
+let bind_type_definition x ts tdef env =
+  let arity = List.length ts in
+  let data_constructors = match tdef with
+    | Abstract -> []
+    | DefineSumType ds -> List.map (fun (k, _) -> Position.value k) ds
+  in
+  let constructor_definition (k, tys) =
+    let atys = List.map (fun ty -> internalize_ty env (Position.value ty)) tys in
+    let scheme =
+      mk_type_scheme (atys --> ATyCon (x, List.map (fun v -> ATyVar v) ts))
+    in
+    (Position.value k, scheme)
+  in
+  let constructors = match tdef with
+    | Abstract -> []
+    | DefineSumType ds -> List.map constructor_definition ds @ env.constructors
+  in
+  let type_constructors =
+    (x, { arity; data_constructors }) :: env.type_constructors
+  in
+  { env with type_constructors; constructors }
+
+exception UnboundConstructor of Position.position * constructor
+
+let lookup_type_scheme_of_constructor pos x env =
+  try
+    List.assoc x env.constructors
+  with Not_found ->
+    raise (UnboundConstructor (pos, x))
+
+let initial_typing_environment () =
+  empty_typing_environment |>
+  List.fold_right (fun ti env -> bind_type_definition ti [] Abstract env) [
+    tcbool; tcunit; tcstring; tcchar; tcint
+  ] |>
+  bind_type_definition (TCon "ref") [TId "'a"] Abstract
+  |> List.fold_right (fun (x, s) env -> bind_value (Id x) (mk_type_scheme s) env) [
+    "true"     , hbool;
+    "false"    , hbool;
+    "nothing"  , hunit;
+    "print_int", [hint] --> hunit;
+    "print_string", [hstring] --> hunit;
+    "print", [tvar "'a"] --> hunit;
+    "`||", [hbool; hbool] --> hbool;
+    "`&&", [hbool; hbool] --> hbool;
+    "`=",  [hint; hint] --> hbool;
+    "`<=", [hint; hint] --> hbool;
+    "`>=", [hint; hint] --> hbool;
+    "`<",  [hint; hint] --> hbool;
+    "`>",  [hint; hint] --> hbool;
+    "`+",  [hint; hint] --> hint;
+    "`*",  [hint; hint] --> hint;
+    "`-",  [hint; hint] --> hint;
+    "`/",  [hint; hint] --> hint;
+  ]
+
+let rec print_aty = function
+  | ATyVar (TId x) ->
+    x
+  | ATyArrow (ins, out) ->
+    String.concat " * " (List.map print_aty' ins)
+    ^ " -> " ^ print_aty' out
+  | ATyCon (TCon x, []) ->
+    x
+  | ATyCon (TCon x, ts) ->
+    x ^ "(" ^ String.concat ", " (List.map print_aty' ts) ^ ")"
+and print_aty' = function
+  | (ATyArrow (_, _)) as t -> "(" ^ print_aty t ^ ")"
+  | x -> print_aty x
+
+let print_binding (Id x, Scheme (_, s)) =
+  x ^ " : " ^ print_aty s
+
+let print_typing_environment tenv =
+  String.concat "\n" (List.map print_binding tenv.values)
 
