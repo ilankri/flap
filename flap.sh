5dd8ce97ae94e5724552036fdbaebe735ec20ba9
Idir Lankri <lankri.idir@gmail.com>
2018-09-01 00:04:28 +0200

    Improve flap.sh script

--- flap.sh
+++ flap.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-rlwrap ./flap -i true -V true -VV true -s $1 -t $2
+
+if [ $# -eq 1 ]; then
+    rlwrap ./flap -i true -V true -VV true -s $1
+else
+    rlwrap ./flap -i true -V true -VV true -s $1 -t $2
+fi
